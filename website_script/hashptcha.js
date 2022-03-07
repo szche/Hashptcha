80ca2a318e4903b5fd8f7e90d874a04983f5b427
szche <github@chadam.pl>
2022-03-07 07:56:04 +0100

    Add server URL

--- website_script/hashptcha.js
+++ website_script/hashptcha.js
@@ -2,15 +2,15 @@ const hashptcha = new class {
 	constructor() {
 		this.div = document.querySelector(".hashptcha");
 		this.site_key = this.div.getAttribute("site-key");
-		this.server_url = "URL_HERE_TBD";
+		this.server_url = "http://127.0.0.1:5000/frame";
 		this.render();
 	}
 
 	render() {
 		this.iframe = document.createElement("iframe");
-		this.iframe.src = this.server_url+'/?'+this.site_key; 
+		this.iframe.src = this.server_url+'?'+this.site_key; 
 		this.iframe.id="HashptchaIframe";
-		this.iframe.width="200px";
+		this.iframe.width="250px";
 		this.iframe.height="60px";
 		this.iframe.scrolling="no";
 		this.iframe.style.border="0px";
