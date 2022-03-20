e3308833729a44ed119ce9b5263e10736a7bb98d
szche <github@chadam.pl>
2022-03-20 19:27:35 +0100

    Improve dashboard and hashes screens

--- management_server/app.py
+++ management_server/app.py
@@ -11,13 +11,46 @@ work_dispatcher = WorkDispatcher()
 # Main admin panel for displaying info
 @app.route("/")  
 def home():
-    return render_template("index.html")
+    all_hashes = database.get_all("hashes")
+    all_hashes_num = len(all_hashes)
+    cracked_hashes = len([x for x in all_hashes if x[3] == 1])
+    all_tasks = database.get_all("tasks")
+    tasks_completed = [x for x in all_tasks if x[1] == 1]
+    tasks_completed_num = len(tasks_completed)
+    hash_counter_dict = {}
+    for task in tasks_completed:
+        hash_id = task[5]
+        pass_point = int(task[6], 2)
+        if hash_id not in hash_counter_dict:
+            hash_counter_dict[hash_id] = pass_point
+        elif hash_counter_dict[hash_id] < pass_point:
+            hash_counter_dict[hash_id] = pass_point
+    data = {
+        "all_hashes": all_hashes,
+        "all_hashes_num": all_hashes_num,
+        "cracked_hashes": cracked_hashes,
+        "all_tasks": all_tasks,
+        "tasks_completed": tasks_completed_num,
+        "all_hashes": sum(list(hash_counter_dict.values()))
+    }
+    return render_template("index.html", data=data)
 
 # Showing hashes in database
 # and form to add new hash
 @app.route("/hashes")  
 def hashes():
-    return render_template("hashes.html", hashes=database.get_all("hashes"), supported_types=SUPPORTED_HASHES)
+    hashes_display = []
+    for h in database.get_all("hashes"):
+        cracked = h[3]
+        plaintext = h[4]
+        tasks_hash_counter = max([int(x[6], 2) for x in database.find_tasks_for_hash(h[0]) if x[1] == 1], default=0)
+        if cracked == 0:
+            hashes_display.append([h[0], h[1], h[2], h[3], h[4], tasks_hash_counter])
+        else:
+            plaintext_int = int(plaintext, 2)
+            plaintext_ascii = plaintext_int.to_bytes((plaintext_int.bit_length() + 7) // 8, 'big').decode()
+            hashes_display.append( [h[0], h[1], h[2], h[3], plaintext_ascii, tasks_hash_counter] )
+    return render_template("hashes.html", hashes=hashes_display, supported_types=SUPPORTED_HASHES)
 
 # Showing websites connected to the service
 # And their secret-public keypairs
